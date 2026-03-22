554e8a87f383437b5e97064538f16128fc930486
Vran29 <121425298+Vran29@users.noreply.github.com>
2026-03-22 20:10:27 +0100

    updated vrnwizard to take files from this new repo

--- repo/plugin.program.vrnwizard/uservar.py
+++ repo/plugin.program.vrnwizard/uservar.py
@@ -1,11 +1,11 @@
 '''#####-----Build File-----#####'''
-buildfile = 'https://raw.githubusercontent.com/Vran29/repository.vrnrepo/refs/heads/master/builds.xml'
+buildfile = 'https://raw.githubusercontent.com/Vran29/repository.vrn/refs/heads/master/repo/plugin.program.vrnwizard/resources/texts/builds.xml'
 
 '''#####-----Videos File-----#####'''
-videos_url = 'http://CHANGEME'
+videos_url = 'https://raw.githubusercontent.com/Vran29/repository.vrn/refs/heads/master/repo/plugin.program.vrnwizard/resources/texts/videos.xml'
 
 '''#####-----Notification File-----#####'''
-notify_url  = 'http://CHANGEME'
+notify_url  = 'https://raw.githubusercontent.com/Vran29/repository.vrn/refs/heads/master/repo/plugin.program.vrnwizard/resources/texts/notify.txt'
 
 '''#####-----Changelog Directory-----#####'''
 changelog_dir  = 'http://CHANGEME/'
