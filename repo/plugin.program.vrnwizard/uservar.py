ddc3752b619beb96e401503e31561b27cc5484b6
Vran29 <121425298+Vran29@users.noreply.github.com>
2026-03-22 20:01:37 +0100

    Basically changed everfything

---
+++ repo/plugin.program.vrnwizard/uservar.py
@@ -0,0 +1,14 @@
+'''#####-----Build File-----#####'''
+buildfile = 'https://raw.githubusercontent.com/Vran29/repository.vrnrepo/refs/heads/master/builds.xml'
+
+'''#####-----Videos File-----#####'''
+videos_url = 'http://CHANGEME'
+
+'''#####-----Notification File-----#####'''
+notify_url  = 'http://CHANGEME'
+
+'''#####-----Changelog Directory-----#####'''
+changelog_dir  = 'http://CHANGEME/'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
